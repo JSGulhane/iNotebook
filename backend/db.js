4f190ed151f2dc5ab411120a8717a65b618eabe1
JSGulhane <jinalsgulhane@gmail.com>
2024-06-12 01:03:50 +0530

    models,schemas,db

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,19 @@
+// const mongoose = require('mongoose');
+
+// const mongoURI = "mongodb://localhost:27017/";
+
+// const connectToMongo = async () => {
+//     await mongoose.connect(mongoURI)
+//         .then(() => console.log("Connected to Mongo successfully"))
+//         .catch(err => console.log("Failed to connect to Mongo:", err));
+// };
+
+// module.exports = connectToMongo;
+
+
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://127.0.0.1:27017/";
 
 const connectToMongo = async () => {
     await mongoose.connect(mongoURI)
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,36 @@
+// const connectToMongo = require('./db');
+// const express = require('express')
+
+// connectToMongo();
+
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Jinal!')
+// })
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
 const connectToMongo = require('./db');
-const express = require('express')
+const express = require('express');
 
 connectToMongo();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.get('/', (req, res) => {
-  res.send('Hello Jinal!')
-})
+  res.send('Hello Jinal!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
